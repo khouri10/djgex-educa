e2670b6a764cea014572dd52e7036a7236eacac0
Marcel <khouri10@gmail.com>
2018-06-02 17:56:36 -0300

    Create custom field to ordering

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
+
 
 class Subject(models.Model):
 	title = models.CharField(max_length=200)
@@ -31,9 +33,13 @@ class Module(models.Model):
 	course = models.ForeignKey(Course, related_name='modules')
 	title = models.CharField(max_length=200)
 	description = models.TextField(blank=True)
+	order = OrderField(blank=True, for_fields=['course'])
+
+	class Meta:
+		ordering = ['order']
 
 	def __str__(self):
-		return self.title
+		return '{}. {}'.format(self.order, self.title)
 
 class Content(models.Model):
 	module = models.ForeignKey(Module, related_name='contents')
@@ -41,6 +47,9 @@ class Content(models.Model):
 		'model__in':('text', 'video', 'image', 	'file')})
 	object_id = models.PositiveIntegerField()
 	item = GenericForeignKey('content_type', 'object_id')
+	order = OrderField(blank=True, for_fields=['module'])
+	class Meta:
+		ordering = ['order']
 
 class ItemBase(models.Model):
 	owner = models.ForeignKey(User, related_name='%(class)s_related')
