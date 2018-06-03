f93a76a9b4f72bd49317d01922625ce0131de6ee
Marcel <khouri10@gmail.com>
2018-06-03 18:46:20 -0300

    Create system for students display content

--- educa/settings.py
+++ educa/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'courses',
     'students',
+    'embed_video',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,3 +106,6 @@ STATIC_URL = '/static/'
 
 from django.core.urlresolvers import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- educa/urls.py
+++ educa/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from courses.views import CourseListView
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     url(r'^$', CourseListView.as_view(), name='course_list'),
     url(r'^students/', include('students.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
